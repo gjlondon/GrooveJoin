0cf65c047ce9fe3fdd8c9737d544f165eb43b361
Rogueleaderr <rogueleaderr@gmail.com>
2012-02-26 08:54:18 -0500

    tweaks

--- groovejoin.js
+++ groovejoin.js
@@ -8,6 +8,11 @@ var list_counter = 0;
 var globalArtistList = [];
 var globalSongList = [];
 var globalURIBlock = [];
+var lastSong = false;
+var lastURI = false;
+var playlistLoaded = false;
+var gUser1;
+var gUser2;
 
 function init() {
     console.log("init()");
@@ -129,9 +134,8 @@ function getJoinedList(list1, list2){
 	rankedArtists.push(joint_list[item][1]);
     }
     
-    var block = document.getElementById('playlist_space');
-    block.innerHTML = rankedArtists;
     globalArtistList = rankedArtists;
+    getSongs();
 return joint_list;
 };
 
@@ -139,6 +143,9 @@ function buildPlaylist(user1,user2) {
 
     var user1 = document.getElementById('user_name').value;
     var user2 = document.getElementById('friend_name').value;
+    gUser1 = user1;
+    gUser2 = user2;
+
     var joint_list = getTopArtists1(user1,user2);
 
 
@@ -154,11 +161,18 @@ function getSongs(){
     var artists = document.getElementById("playlist_space");
 //    console.log(artists);
 //    console.log(globalArtistList);
+    var artLen = globalArtistList.length;
+    console.log("There will be "+artLen+" artists.");
     for (i=0;i<globalArtistList.length;i++){
+	if (i===(artLen-1)){
+	    lastSong = true;
+	    console.log("just set lastsong to true because the counter is at "+i);
+	    }
 	var song = getSong(globalArtistList[i]);
 
 	}
-    console.log(globalSongList);
+    //console.log(globalSongList);
+   
 }
 function getSong(artist){
     
@@ -171,7 +185,7 @@ function getSong(artist){
 
 	req.onreadystatechange = function() {
 
-		console.log(req.status);
+	//	console.log(req.status);
 
    		if (req.readyState == 4) {
     		    if (req.status == 200) {
@@ -183,9 +197,13 @@ function getSong(artist){
 			    globalSongList.push([list[i].name,artist]);
 			    
 			    }
-			var block = document.getElementById('song_space');
+	//		var block = document.getElementById('song_space');
 			shuffle(globalSongList);
-			block.innerHTML = globalSongList;
+	//		block.innerHTML = globalSongList;
+			if (lastSong === true){
+			    console.log("it's the last song"); 
+			    getSpotifyURIs();
+			    }
 
 			    
 	
@@ -211,16 +229,28 @@ function shuffle(list) {
 function getSpotifyURIs(){
      
 
-    
-    for (item in globalSongList){
+
+    var songLen = globalSongList.length;
+    console.log("there are "+songLen+" songs");
+
+    for (item=0; item< globalSongList.length; item++){
 	var artist = globalSongList[item][1]
 //	console.log("artist: "+artist);
 	var track = globalSongList[item][0]
 //	console.log("track: "+track);
 	var params = "track.getPlaylinks&artist[]="+encodeURI(artist)+"&track[]="+encodeURI(track)
 	var api_request = last_endpoint+params;
+	console.log(item+" is item");
+	if (item===(songLen.length-1)){
+	    lastURI=true;
+	    }
+	else{
+	    lastURI=false;
+	    }
 	getSpotifyURI(api_request);
+	
     }
+
     
 }
 function getSpotifyURI(api_request){
@@ -231,17 +261,25 @@ function getSpotifyURI(api_request){
 
 	req.onreadystatechange = function() {
 
-		console.log(req.status);
+	//	console.log(req.status);
 
    		if (req.readyState == 4) {
     		    if (req.status == 200) {
-       			console.log("Search complete!");
+       	//		console.log("Search complete!");
 			var obj = JSON.parse(req.responseText);
 			var list = obj.spotify;
-			console.log(list);
+	//		console.log(list);
 			globalURIBlock.push(list.track.externalids.spotify);
-			console.log("some stuff");
-
+			console.log("some stuff"+lastURI);
+	//		if (lastURI===true){
+			    
+	//		    if(playlistLoaded===false){
+				var block = document.getElementById('song_space');
+				block.innerHTML = "<ul>"+globalSongList+"</ul>";
+//	var block = get
+				//	loadPlaylist();
+	//			}
+	//		}
 
 		//	console.log(globalURIBlock);
 
@@ -259,8 +297,8 @@ function getSpotifyURI(api_request){
 function loadPlaylist(){
     
     var now = new Date();
-
-    var playlist = new models.Playlist("GrooveJoin" + now.toDateString() +" "+ now.toLocaleTimeString())//fromURI("spotify:user:rogueleaderr:playlist:2EZclFWU67yAInmXBKUJQB");
+    playlistLoaded = true;
+    var playlist = new models.Playlist("GrooveJoin " +gUser1+"/"+gUser2+" "+ now.toDateString() +" "+ now.toLocaleTimeString())//fromURI("spotify:user:rogueleaderr:playlist:2EZclFWU67yAInmXBKUJQB");
     var tracks = playlist.tracks;
     for (track in tracks){
 	try{
